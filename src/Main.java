52c0770d573186c005ed05c9c166622ba2313635
Ines <ines.cvek16@gmail.com>
2025-03-05 20:07:28 +0100

    remove unused

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testNum = 0;
 
